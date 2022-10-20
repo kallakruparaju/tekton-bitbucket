54550f7432cccab8b02b5b0859ae5ad5bfba4f67
Kalla Kruparaju <kallakruparaju@gmail.com>
2022-10-20 18:54:28 +0530

    tekton-bitbucket

---
+++ execute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+kubectl apply  -f test.yaml
+kubectl apply  -f build.yaml
+kubectl apply  -f prod.yaml
+#kubectl apply  -f ci-cd-pipeline.yaml
+#kubectl apply  -f ci-cd-pipeline-run.yaml
+kubectl apply  -f secret.yaml
+kubectl apply  -f rbac.yaml
+kubectl apply  -f trigger-binding.yaml
+kubectl apply  -f trigger-template.yaml
+kubectl apply  -f event-listener.yaml
